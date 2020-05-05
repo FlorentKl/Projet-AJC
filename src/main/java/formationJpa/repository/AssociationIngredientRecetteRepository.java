0901d3a64caaea089fa93d193e620db8a2510812
FlorentKl <florent.klee@free.fr>
2020-05-05 20:40:54 +0200

    oubli d'ajout

--- src/main/java/formationJpa/repository/AssociationIngredientRecetteRepository.java
+++ src/main/java/formationJpa/repository/AssociationIngredientRecetteRepository.java
@@ -5,11 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import formationJpa.entity.Ingredients.AssociationIngredientRecette;
 import formationJpa.entity.Ingredients.AssociationIngredientRecetteKey;
 
-<<<<<<< HEAD
 public interface AssociationIngredientRecetteRepository extends JpaRepository<AssociationIngredientRecette, AssociationIngredientRecetteKey> {
-=======
-public interface AssociationIngredientRecetteRepository
-        extends JpaRepository<AssociationIngredientRecette, AssociationIngredientRecetteKey> {
->>>>>>> florent
 
 }
