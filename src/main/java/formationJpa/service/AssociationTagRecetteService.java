079cd7496bb3758fb29d9b31821fb7017f7fb3d5
mfong28 <moka.fong@gmail.com>
2020-05-05 16:47:07 +0200

    ajout EtapeRecette et AssoTagRecette Service

--- src/main/java/formationJpa/service/AssociationTagRecetteService.java
+++ src/main/java/formationJpa/service/AssociationTagRecetteService.java
@@ -1,9 +1,39 @@
 package formationJpa.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import formationJpa.entity.tag.AssociationTagRecette;
+import formationJpa.entity.tag.AssociationTagRecetteKey;
+import formationJpa.repository.AssociationTagRecetteRepository;
+
+@Service
 public class AssociationTagRecetteService {
-	// TODO - ajout
-	// TODO - mise à jour
-	// TODO - delete
-	// TODO - deleteByKey
-	// TODO - findById
+	
+	@Autowired
+	private AssociationTagRecetteRepository associationTagRecetteRepository;
+	
+	
+	public void insert(AssociationTagRecette associationTagRecette) {
+		associationTagRecetteRepository.save(associationTagRecette);	
+	}
+	
+		
+	
+	public void delete(AssociationTagRecette associationTagRecette) {
+		associationTagRecetteRepository.delete(associationTagRecette);
+	}
+	
+	
+	public void deleteById(AssociationTagRecetteKey associationTagRecetteKey) {
+		 associationTagRecetteRepository.deleteById(associationTagRecetteKey);
+	}
+	
+	
+	public Optional<AssociationTagRecette> findById(AssociationTagRecetteKey associationTagRecetteKey) {
+		return associationTagRecetteRepository.findById(associationTagRecetteKey);
+		
+	}
 }
--- src/main/java/formationJpa/service/EtapeRecetteService.java
+++ src/main/java/formationJpa/service/EtapeRecetteService.java
@@ -1,9 +1,57 @@
 package formationJpa.service;
 
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import formationJpa.entity.recette.EtapeRecette;
+import formationJpa.exception.NoEtapeRecetteException;
+import formationJpa.repository.EtapeRecetteRepository;
+
+
+@Service
 public class EtapeRecetteService {
-	// TODO - ajout
-	// TODO - mise à jour
-	// TODO - delete
-	// TODO - deleteByKey
-	// TODO - findById
+	
+	@Autowired
+	private EtapeRecetteRepository etapeRecetteRepository;
+	
+	
+	public void insert(EtapeRecette etapeRecette) throws NoEtapeRecetteException {
+		if(etapeRecette.getText().isEmpty()) {
+			throw new NoEtapeRecetteException();
+		}
+		etapeRecetteRepository.save(etapeRecette);		
+	}
+	
+	public EtapeRecette update(EtapeRecette etapeRecette) {
+		Optional<EtapeRecette> opt=etapeRecetteRepository.findById(etapeRecette.getId());
+		if (opt.isPresent()) {
+			EtapeRecette etapeRecetteEnBase=opt.get();
+			if(etapeRecette.getText()!=null) {
+				etapeRecetteEnBase.setText(etapeRecette.getText());
+			}
+			if(etapeRecette.getPhoto()!=null) {
+				etapeRecetteEnBase.setPhoto(etapeRecette.getPhoto());
+			}
+			etapeRecetteRepository.save(etapeRecetteEnBase);
+			return etapeRecetteEnBase;
+		
+		}else {
+			return null;
+		}
+	}
+	
+	public void delete(EtapeRecette etapeRecette) {
+		delete(etapeRecette);
+	}
+	public void deleteById(Integer id) {
+		etapeRecetteRepository.deleteById(id);
+		
+	}
+		
+	public Optional<EtapeRecette> findById(Integer id) {
+		return etapeRecetteRepository.findById(id);
+	}
 }
