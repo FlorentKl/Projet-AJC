ae34b3bf139f4f7fa164678bde279186580c0eec
mfong28 <moka.fong@gmail.com>
2020-06-01 23:49:33 +0200

    modif dans model image

--- angular/src/app/model/image-model.ts
+++ angular/src/app/model/image-model.ts
@@ -1,2 +1,36 @@
 export class ImageModel {
+  constructor(private _id: number, private _nom: string, private _type: string, private _image: any) {
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get nom(): string {
+    return this._nom;
+  }
+
+  set nom(value: string) {
+    this._nom = value;
+  }
+
+  get type(): string {
+    return this._type;
+  }
+
+  set type(value: string) {
+    this._type = value;
+  }
+
+  get image(): any {
+    return this._image;
+  }
+
+  set image(value: any) {
+    this._image = value;
+  }
 }
