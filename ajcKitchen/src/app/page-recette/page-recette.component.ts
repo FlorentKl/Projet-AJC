c58d99c9965f1cd7e0f206ce0c594ff2d78b57bd
mfong28 <moka.fong@gmail.com>
2020-06-01 10:28:13 +0200

    debut index

---
+++ ajcKitchen/src/app/page-recette/page-recette.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-recette',
+  templateUrl: './page-recette.component.html',
+  styleUrls: ['./page-recette.component.css']
+})
+export class PageRecetteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
