fef55118b78a30eaae67f11e4559bfc46adc9305
FlorentKl <florent.klee@free.fr>
2020-06-02 17:10:58 +0200

    oups

--- Boot/src/main/java/projetSpringBoot/service/recette/RecetteService.java
+++ Boot/src/main/java/projetSpringBoot/service/recette/RecetteService.java
@@ -3,6 +3,8 @@ package projetSpringBoot.service.recette;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import projetSpringBoot.model.recette.Recette;
 import projetSpringBoot.service.GenericInterfaceService;
 
