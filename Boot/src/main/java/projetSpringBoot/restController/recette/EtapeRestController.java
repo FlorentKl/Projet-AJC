6cef2731af076d56578dc1149364a53d8fa81b54
FlorentKl <florent.klee@free.fr>
2020-06-02 15:06:04 +0200

    Ajout RestController pour étapes de la recette

--- Boot/src/main/java/projetSpringBoot/restController/recette/EtapeRestController.java
+++ Boot/src/main/java/projetSpringBoot/restController/recette/EtapeRestController.java
@@ -1,8 +1,36 @@
-// package projetSpringBoot.restController.recette;
+package projetSpringBoot.restController.recette;
 
-// @RestController
-// @RequestMapping("/rest/etape")
-// @CrossOrigin(origins = "*")
-// public class EtapeRestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
-// }
+import projetSpringBoot.model.recette.EtapeRecette;
+import projetSpringBoot.service.EtapeRecetteService;
+
+@RestController
+@RequestMapping("/rest/etape")
+@CrossOrigin(origins = "*")
+public class EtapeRestController {
+    @Autowired
+    EtapeRecetteService etapeService;
+
+    @PostMapping(value = { "", "/" })
+    public ResponseEntity<Void> addEtapes(@RequestBody EtapeRecette[] etapes, BindingResult br,
+            UriComponentsBuilder uCB) {
+        if (br.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        for (EtapeRecette etape : etapes) {
+            etapeService.insert(etape);
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+}
