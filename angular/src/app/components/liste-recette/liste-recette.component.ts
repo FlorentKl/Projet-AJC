527b8c4329eb5cce6791df5acecab417e2a9a490
mfong28 <moka.fong@gmail.com>
2020-06-04 15:59:17 +0200

    modifs recherche avancée

--- angular/src/app/components/liste-recette/liste-recette.component.ts
+++ angular/src/app/components/liste-recette/liste-recette.component.ts
@@ -13,8 +13,16 @@ import {Cout} from '../../model/cout.enum';
 export class ListeRecetteComponent implements OnInit {
   private _recettes: Recette[];
 
-  namelike: string;
-  searchUrl = 'http://localhost:8080/web/rest/recette/search';
+  private _namelike: string = '';
+  private _note: number = null;
+  private _type: any = '';
+  private _difficulte: any = '';
+  private _cout: any='';
+  private _nodifficulte: any = '';
+  private _nocout: any = '';
+
+
+  private _searchUrl = 'http://localhost:8080/web/rest/recette/search';
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -27,16 +35,41 @@ export class ListeRecetteComponent implements OnInit {
 
   private initListe() {
     this.activatedRoute.queryParams.subscribe((params) => {
-      if (params.namelike) {
+      if (params.namelike){
         this.namelike = params.namelike;
       }
-      let truc = new HttpParams().set('namelike', this.namelike);
+      if (params.note){
+        this.note = params.note;
+      }
+      if (params.type){
+        this.type = params.type;
+      }
+      if (params.difficulte){
+        this.difficulte = params.difficulte;
+      }
+      if (params.cout){
+        this.cout = params.cout;
+      }
+      if (params.nodifficulte){
+        this.nodifficulte = params.nodifficulte;
+      }
+      if (params.nocout){
+        this.nocout = params.nocout;
+      }
+      const paramsget = new HttpParams()
+        .set('namelike', this._namelike)
+        .set('note', this._note.toString())
+        .set('type', this._type)
+        .set('difficulte', this._difficulte)
+        .set('cout', this._cout)
+        .set('nodifficulte', this._nodifficulte)
+        .set('nocout', this._nocout);
 
-      this.httpClient.get(this.searchUrl, { params: truc }).subscribe(
+      this.httpClient.get(this._searchUrl, { params: paramsget}).subscribe(
         (res) => {
           console.log(res);
           this.recettes = res as Recette[];
-          for (let recette of this.recettes) {
+          for (const recette of this.recettes) {
             recette.difficulte = 'Difficulte : ' + Difficulte[recette.difficulte];
             recette.cout = 'Cout : ' + Cout[recette.cout];
           }
@@ -54,5 +87,69 @@ export class ListeRecetteComponent implements OnInit {
 
   public set recettes(v: Recette[]) {
     this._recettes = v;
+  }
+
+  get namelike(): string {
+    return this._namelike;
+  }
+
+  set namelike(value: string) {
+    this._namelike = value;
+  }
+
+  get note(): number {
+    return this._note;
+  }
+
+  set note(value: number) {
+    this._note = value;
+  }
+
+  get type(): any {
+    return this._type;
+  }
+
+  set type(value: any) {
+    this._type = value;
+  }
+
+  get difficulte(): any {
+    return this._difficulte;
+  }
+
+  set difficulte(value: any) {
+    this._difficulte = value;
+  }
+
+  get cout(): any {
+    return this._cout;
+  }
+
+  set cout(value: any) {
+    this._cout = value;
+  }
+
+  get nodifficulte(): any {
+    return this._nodifficulte;
+  }
+
+  set nodifficulte(value: any) {
+    this._nodifficulte = value;
+  }
+
+  get nocout(): any {
+    return this._nocout;
+  }
+
+  set nocout(value: any) {
+    this._nocout = value;
+  }
+
+  get searchUrl(): string {
+    return this._searchUrl;
+  }
+
+  set searchUrl(value: string) {
+    this._searchUrl = value;
   }
 }
